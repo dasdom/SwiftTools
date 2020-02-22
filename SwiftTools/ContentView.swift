317ddbf0522a7e61b1b2f8ce927b3fb34bdf9ac4
Dominik Hauser <dom@dasdom.de>
2020-02-22 17:15:13 +0100

    Initial Commit

---
+++ SwiftTools/ContentView.swift
@@ -0,0 +1,19 @@
+//  Created by dasdom on 22.02.20.
+//  Copyright © 2020 dasdom. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+            .frame(maxWidth: .infinity, maxHeight: .infinity)
+    }
+}
+
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
